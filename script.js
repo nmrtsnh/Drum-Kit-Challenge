6ba1c3987654badc81054d6982c514389348b047
Namrata Sinha <68506888+nmrtsnh@users.noreply.github.com>
2021-08-23 14:50:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+}
+
+window.addEventListener("keydown", playSound);
